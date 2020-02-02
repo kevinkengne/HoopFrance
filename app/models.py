200678f3b824df7842645777cc3349906531e600
Kevin <kevin.kengne1@gmail.com>
2020-02-02 16:44:51 +0000

    [HOOP-2] add navigation bar

--- app/models.py
+++ app/models.py
@@ -3,12 +3,12 @@ from django.db import models
 class Team(models.Model):
     team_id = models.IntegerField(db_column='id', primary_key=True)
     city = models.CharField(max_length=13)
-    full_name = models.CharField(db_column='fullName', max_length=22)  # Field name made lowercase.
-    nick_name = models.CharField(db_column='nickName', max_length=13)  # Field name made lowercase.
+    full_name = models.CharField(db_column='fullName', max_length=22)
+    nick_name = models.CharField(db_column='nickName', max_length=13)
     logo = models.CharField(max_length=138)
-    short_name = models.CharField(db_column='shortName', max_length=3)  # Field name made lowercase.
-    conf_name = models.CharField(db_column='confName', max_length=4)  # Field name made lowercase.
-    div_name = models.CharField(db_column='divName', max_length=9)  # Field name made lowercase.
+    short_name = models.CharField(db_column='shortName', max_length=3)
+    conf_name = models.CharField(db_column='confName', max_length=4)
+    div_name = models.CharField(db_column='divName', max_length=9)
 
     def __str__(self):
         return self.full_name
@@ -19,16 +19,16 @@ class Team(models.Model):
 
 class Player(models.Model):
     id = models.IntegerField(primary_key=True)
-    first_name = models.CharField(db_column='firstName', max_length=12)  # Field name made lowercase.
-    last_name = models.CharField(db_column='lastName', max_length=18, blank=True, null=True)  # Field name made lowercase.
-    team = models.ForeignKey(Team, db_column='teamId', on_delete=models.CASCADE)  # Field name made lowercase.
-    years_pro = models.IntegerField(db_column='yearsPro')  # Field name made lowercase.
-    college_name = models.CharField(db_column='collegeName', max_length=34, blank=True, null=True)  # Field name made lowercase.
+    first_name = models.CharField(db_column='firstName', max_length=12)
+    last_name = models.CharField(db_column='lastName', max_length=18, blank=True, null=True)
+    team = models.ForeignKey(Team, db_column='teamId', on_delete=models.CASCADE)
+    years_pro = models.IntegerField(db_column='yearsPro')
+    college_name = models.CharField(db_column='collegeName', max_length=34, blank=True, null=True)
     country = models.CharField(max_length=32, blank=True, null=True)
-    dob = models.DateField(db_column='DOB', blank=True, null=True)  # Field name made lowercase.
+    dob = models.DateField(db_column='DOB', blank=True, null=True)
     affiliation = models.CharField(max_length=50, blank=True, null=True)
-    height_in_meters = models.DecimalField(db_column='heightInMeters', max_digits=4, decimal_places=2, blank=True, null=True)  # Field name made lowercase.
-    weight_in_kilograms = models.DecimalField(db_column='weightInKilograms', max_digits=5, decimal_places=1, blank=True, null=True)  # Field name made lowercase.
+    height_in_meters = models.DecimalField(db_column='heightInMeters', max_digits=4, decimal_places=2, blank=True, null=True)
+    weight_in_kilograms = models.DecimalField(db_column='weightInKilograms', max_digits=5, decimal_places=1, blank=True, null=True)
     pos = models.CharField(max_length=3, blank=True, null=True)
 
     def __str__(self):
--- app/views.py
+++ app/views.py
@@ -3,7 +3,6 @@ from django.http import HttpResponse
 from django.template import loader
 from .models import Player, Team
 
-# Create your views here.
 def index(request):
     return HttpResponse("HoopFrance index.")
 
